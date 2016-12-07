20595e01609d1983b81c3151a26498e3e6238e07
AMU\vb023528 <richard_che-so@ch.honda.com>
2016-12-07 11:54:48 -0500

    Update room.js add parseAuthHeader method

--- controllers/room.js
+++ controllers/room.js
@@ -5,8 +5,27 @@ const passport = require('passport');
 const Room = require('../models/Room');
 const User = require('../models/User');
 
+var parseAuthHeader = function(req,res) {
+	var auth = req.headers['authorization'];  // auth is in base64(username:password)  so we need to decode the base64
+	console.log("Authorization Header is: ", auth);
+	var tmp = auth.split(' ');   // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
+	var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
+	var plain_auth = buf.toString();        // read it back out as a string
+	console.log("Decoded Authorization ", plain_auth);
+	// At this point plain_auth = "username:password"
+	var creds = plain_auth.split(':');      // split on a ':'
+	var username = creds[0];
+	var password = creds[1];
+	console.log("username="+username+ " password="+password);
+	return username;
+};
+
 exports.testBasicAuth = (req, res, next) => {
-    res.json({ "username": req.user.username });
+	email = parseAuthHeader(req,res);
+	User.findOne({ email: email }, (err, user) => {
+	    if (err) { return next(err)};  
+        res.send(user);
+	});
 };
 exports.findAll = (req, res, next) => {
     res.json({ username: req.user.username, email: user.email });
